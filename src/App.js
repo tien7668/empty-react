288cbeb150dbe8defba4d6a951b2a37bad9637dc
tien7668 <tien7668@gmail.com>
2020-11-02 11:12:53 +0700

    6th

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Edited 5th
+          Learn React Edited 6th
         </a>
       </header>
     </div>
